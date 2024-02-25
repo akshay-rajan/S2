307274af97ef4b58ee4309c77800f3d2b6b50dae
Akshay Rajan <akshayrajan345@gmail.com>
2024-02-25 11:24:15 +0530

    Modify Complex Addition

--- OOP/Complex.java
+++ OOP/Complex.java
@@ -1,35 +1,48 @@
 import java.util.Scanner;
 
-public class Complex {
-	int a, b;
 
-	public void print() {
-		System.out.println(this.a + " + " + this.b + "i");
+public class Complex {
+	
+	double a;
+	double b;
+
+	// Constructor
+	Complex(double a, double b) {
+		this.a = a;
+		this.b = b;
 	}
 
-	public void add(Complex other) {
-		Complex ans = new Complex();
-		ans.a = this.a + other.a;
-		ans.b = this.b + other.b;
-		ans.print();
+	// String representation of a complex number
+	public String toString() {
+		return a + " + " + b + "i";
+	}
+
+	// Static method to add two complex numbers
+	public static void add(Complex c1, Complex c2) {
+		Complex ans = new Complex(c1.a + c2.a, c1.b + c2.b);
+		System.out.println("c1 + c2:\t" + ans);
 	}
     
 	public static void main(String[] args) {
-		Complex c1 = new Complex();
-		Complex c2 = new Complex();
 		
+		// Read two complex numbers
+		double a, b;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("c1.a: ");
-		c1.a = sc.nextInt();
-		System.out.println("c1.b: ");
-		c1.b = sc.nextInt();
-		System.out.println("c2.a: ");
-		c2.a = sc.nextInt();
-		System.out.println("c2.b: ");
-		c2.b = sc.nextInt();
-		c1.print();
-		c2.print();
-		c1.add(c2);
+		System.out.print("c1.a: ");
+		a = sc.nextDouble();
+		System.out.print("c1.b: ");
+		b = sc.nextDouble();
+		Complex c1 = new Complex(a, b);
+		System.out.print("c2.a: ");
+		a = sc.nextDouble();
+		System.out.print("c2.b: ");
+		b = sc.nextDouble();
+		Complex c2 = new Complex(a, b);
 		sc.close();
+		
+		// Add the complex numbers
+		System.out.println("c1:\t\t" + c1 + "\nc2:\t\t" + c2);
+		Complex.add(c1, c2);
+
 	}
 }
