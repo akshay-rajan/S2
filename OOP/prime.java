f71c4a76f9e8dcc39b5331b8048cf3a875c6d61c
Akshay Rajan <akshayrajan345@gmail.com>
2024-02-25 11:47:43 +0530

    Modify Prime

--- OOP/prime.java
+++ OOP/prime.java
@@ -1,18 +1,16 @@
 
-class prime {
+class Prime {
 
     public static void main(String[] args) {
         int n = 2;
         int flag = 0;
         for (int i = 2; i < n; i++) {
-                    if (n % i == 0) {
-                                flag++;
-                    }
+            if (n % i == 0) {
+                flag++;
+            }
         } 
-        if (flag == 0) {
-                    System.out.print("Prime");          
-        } else {
-                    System.out.println("Not Prime");  
-        }
+        
+        String ans = flag == 0 ? "Prime" : "Not Prime";
+        System.out.println(ans);
     }
 }
