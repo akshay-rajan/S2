b7c99b74e5401cc0e754813c618e273ace0c49ba
Akshay R <akshayrajan345@gmail.com>
2024-02-27 15:36:53 +0530

    Use Join

--- ADBMS/exercise2.sql
+++ ADBMS/exercise2.sql
@@ -11,7 +11,7 @@ SELECT * FROM employees WHERE last_name="Fay";
 SELECT hire_date FROM employees WHERE last_name="Grant" OR last_name="Whalen";
 
 -- 05
-SELECT * FROM employees WHERE job_id=(SELECT job_id FROM jobs WHERE job_title="Shipping Clerk");
+SELECT * FROM employees JOIN jobs ON jobs.job_id = employees.job_id WHERE job_title="Shipping Clerk";
 
 -- 06
 SELECT * FROM employees WHERE department_id=8;
@@ -38,8 +38,8 @@ SELECT 	first_name, last_name FROM employees WHERE YEAR(hire_date) = 1995;
 INSERT INTO employees VALUES (234, "Paul", "Newton", "paulnewton@gmail.com", "234824873", "1856-02-12", 4, 12000, 100, 11, 231);
 
 -- 14
-DELETE FROM Dept WHERE department_id=5;
 set sql_safe_updates = 0;
+DELETE FROM Dept WHERE department_name="Shipping";
 
 -- 15
 SELECT * FROM employees WHERE DAYNAME(hire_date) = "Thursday";
