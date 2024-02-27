7d390a2d57b7c873ef29236710ef04f682637216
Akshay Rajan <akshayrajan345@gmail.com>
2024-02-27 11:48:32 -0500

    Fix automated commit script

--- ADBMS/test.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE database_name;
-USE database_name;
-CREATE TABLE table_name (
-    column1 DATATYPE CONSTRAINT,
-    column2 DATATYPE CONSTRAINT,
-    ...
-);
-SELECT * FROM table_name;
