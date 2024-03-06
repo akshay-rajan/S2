803f95fac1cefd8db3a878d51786c23154c62311
akshay-rajan <akshayrajan345@gmail.com>
2024-03-06 15:02:34 +0530

    String

--- OOP/StringManipulation.java
+++ OOP/StringManipulation.java
@@ -3,17 +3,13 @@ public class StringManipulation {
 	public static void main(String[] args) {
 
 		String s = "Hello, world!";
-		System.out.println("Original string: " + s);
 
-		// Convert to uppercase using the toUpperCase() method
+		System.out.println("Original string: " + s);
 		String uc_s = s.toUpperCase();
 		System.out.println("\nUppercase string: " + uc_s);
-
-		// Convert to lowercase using the toLowerCase() method
 		String lc_s = s.toLowerCase();
 		System.out.println("\nLowercase string: " + lc_s);
-
-		int index = s.indexOf('o'); // Find first occurrence of 'o' in the string
+		int index = s.indexOf('o');
 		if (index != -1) 
 			System.out.println("\nFirst occurrence of 'o': " +	 index);
 		else
@@ -22,6 +18,8 @@ public class StringManipulation {
 		// Use substring() method to extract part of a string
 		String sub_s = s.substring(7, 12); // Extract characters from position 7 to 12
 		System.out.println("\nSubstring from position 7 to 12: " + sub_s);
+
+
 	}
 
 }
