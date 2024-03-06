67cc030b245f4d507c3917d6e904de8ac424a1eb
akshay-rajan <akshayrajan345@gmail.com>
2024-03-06 15:36:41 +0530

    compareTo

--- OOP/StringManipulation.java
+++ OOP/StringManipulation.java
@@ -5,19 +5,36 @@ public class StringManipulation {
 		String s = "Hello, world!";
 
 		System.out.println("Original string: " + s);
-		String uc_s = s.toUpperCase();
-		System.out.println("\nUppercase string: " + uc_s);
 		String lc_s = s.toLowerCase();
 		System.out.println("\nLowercase string: " + lc_s);
-		int index = s.indexOf('o');
-		if (index != -1) 
-			System.out.println("\nFirst occurrence of 'o': " +	 index);
-		else
-			System.out.println("\n'o' not found");
-
-		// Use substring() method to extract part of a string
-		String sub_s = s.substring(7, 12); // Extract characters from position 7 to 12
-		System.out.println("\nSubstring from position 7 to 12: " + sub_s);
+		String uc_s = s.toUpperCase();
+		System.out.println("\nUppercase string: " + uc_s);
+		String re_s = s.replace("o", "e");
+		System.out.println("\nReplaced string: " + re_s);
+		
+		String s1 = "Hello";
+		String s2 = "hello";
+		if (s1.equals(s2)) {
+			System.out.println("The strings are equal!");
+		} else {
+			System.out.println("The strings are not equal!");
+		}
+		if (s1.equalsIgnoreCase(s2)) {
+			System.out.println("The strings are equal case-insensitively!");
+		} else {
+			System.out.println("The strings are not equal case-insensitively!");
+		}
+		
+		System.out.println("The length of the string is " + s.length());
+		System.out.println("The character at position 5 is " + s.charAt(5));
+		if (s1.compareTo(s2) < 1) {
+			System.out.println(s1 + "is less than " + s2);
+		} else if (s1.compareTo(s2) > 1) {
+			System.out.println(s1 + "is greater than " + s2);
+		} else {
+			System.out.println("The strings are equal");
+		}
+		
 
 
 	}
