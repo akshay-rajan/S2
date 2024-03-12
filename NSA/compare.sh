9ab103beb4cf3ef3074e636bc2a9a4bdfae57896
akshay-rajan <akshayrajan345@gmail.com>
2024-03-12 05:05:52 -0400

    Selective Compare

--- NSA/compare.sh
+++ NSA/compare.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # Compare the data in two files as 3 columns: 
-# Matching items in file1, but not in file2 | In file2 but not in file1 | Common items
+# Items in file1, but not in file2 (Non matching items in first file) | Non matching items in second file | Common / Matching items
 comm file1.txt file2.txt
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+# To display only the 2nd column
+comm -13 file1.txt file2.txt
