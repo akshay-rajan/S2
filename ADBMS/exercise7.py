8d9109a33ae1dea21f0990b0505cb014aa3e6584
akshay-rajan <akshayrajan345@gmail.com>
2024-04-03 11:42:02 +0530

    Add q2

--- ADBMS/exercise7.py
+++ ADBMS/exercise7.py
@@ -1,6 +1,14 @@
 import pymongo
 
 url = "mongodb://localhost:27017"
-
 client = pymongo.MongoClient(url)
-print(client)
+db  = client["college"]
+collection = db["studlist"]
+
+print("1. Name and Mark of all female students in MCA: ")
+for student in collection.find({"gender": "female", "course": "MCA"}):
+    print("Name:", student['name']['fname'], student['name']['lname'])
+    print("Mark:", student['mark'])
+
+print("2. Student who scored the highest mark in MCA: ")
+highest_score = max(student['mark'] for student in collection.find({'course': 'MCA'}))
--- ADBMS/mongo/exercise7.py
+++ ADBMS/mongo/exercise7.py
@@ -4,4 +4,11 @@ url = "mongodb://localhost:27017"
 client = pymongo.MongoClient(url)
 db  = client["college"]
 collection = db["studlist"]
-print(collection)
+
+print("1. Name and Mark of all female students in MCA: ")
+for student in collection.find({"gender": "female", "course": "MCA"}):
+    print("Name:", student['name']['fname'], student['name']['lname'])
+    print("Mark:", student['mark'])
+
+print("2. Student who scored the highest mark in MCA: ")
+highest_score = max(student['mark'] for student in collection.find({'course': 'MCA'}))
