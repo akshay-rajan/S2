799d3aaab01efcb79802e43de66fcf9a9f05a8fc
Akshay Rajan <akshayrajan345@gmail.com>
2024-04-24 21:25:55 +0530

    Q1

--- OOP/CO1/Product.java
+++ OOP/CO1/Product.java
@@ -1,3 +1,5 @@
+// 1. Define a class 'product' with data members pcode, pname and price. Create 3 objects of the class and find the product having the lowest price.
+
 import java.util.Scanner;
 
 public class Product {
