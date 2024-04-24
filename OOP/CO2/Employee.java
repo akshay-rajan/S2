8f3989ea16ab963155e2f3be6265a6cc7b3313a8
Akshay Rajan <akshayrajan345@gmail.com>
2024-04-24 21:47:12 +0530

    Q4

--- OOP/CO2/Employee.java
+++ OOP/CO2/Employee.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
+// 4. Program to create a class for Employee having attributes eNo, eName eSalary. Read n employ information and Search for an employee given eNo, using the concept of Array of Objects.
 
+import java.util.Scanner;
 
 public class Employee {
     
