abcb743b95e4d62c40d51cde24d6f549c001b59a
Akshay Rajan <akshayrajan345@gmail.com>
2024-04-24 22:53:15 +0530

    Add Q1

---
+++ OOP/CO4/Graphics/Rectangle.java
@@ -0,0 +1,20 @@
+package Graphics;
+
+// Import Abstract Class
+import Graphics.Shape;
+
+public class Rectangle implements Shape {
+
+    int length;
+    int breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double area() {
+        return length * breadth;
+    }
+
+}
---
+++ OOP/CO4/GraphicsPkg.java
@@ -0,0 +1,12 @@
+import Graphics.*;
+
+public class GraphicsPkg {
+    
+    public static void main(String[] args) {
+        
+        Rectangle r = new Rectangle(3, 4);
+        System.err.println(r.area());
+
+    }
+
+}
