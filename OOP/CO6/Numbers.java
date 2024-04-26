e22885da7aacaa458afcb7ceb8fac008963ec667
Akshay Rajan <akshayrajan345@gmail.com>
2024-04-26 21:47:03 +0530

    Q4 Add Numbers to different files Complete

--- OOP/CO6/Numbers.java
+++ OOP/CO6/Numbers.java
@@ -1,5 +1,6 @@
 // 4. Write a program that reads from a file having integers. Copy even numbers and odd numbers to separate files.
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,11 +11,33 @@ public class Numbers {
     public static void main(String[] args) {
         
         try {
-            FileWriter w = new FileWriter("nums.txt");
-            for (int i = 0; i < 100; i += 3) {
-                w.write(Integer.toString(i) + "\n");
+
+            // To read line by line
+            FileReader reader = new FileReader("integers.txt");
+            BufferedReader integers = new BufferedReader(reader);
+
+            FileWriter even = new FileWriter("integers-even.txt");
+            FileWriter odd = new FileWriter("integers-odd.txt");
+
+            String line = integers.readLine();
+            while (line != null) {
+                int num = Integer.valueOf(line);
+                
+                // Write to different files according to the parity
+                if (num % 2 == 0) {
+                    even.write(Integer.toString(num) + "\n");
+                } else {
+                    odd.write(Integer.toString(num) + "\n");
+                }
+
+                System.out.println(num);
+                line = integers.readLine();
             }
-            w.close();
+
+            integers.close();
+            even.close();
+            odd.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
