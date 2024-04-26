7d404eac6c0da3b3942170210c5975f3585aa305
Akshay Rajan <akshayrajan345@gmail.com>
2024-04-26 19:44:40 +0530

    Update Q6 Syntax

--- OOP/CO6/CopyFiles.java
+++ OOP/CO6/CopyFiles.java
@@ -15,15 +15,15 @@ public class CopyFiles {
     public static void main(String[] args) {
         
         try {
-            // Read from f1 and write into f2
             FileReader f1 = new FileReader("file1.txt");
             FileWriter f2 = new FileWriter("file2.txt");
 
-            int line = f1.read();
-            while (line != -1) {
-                System.out.print((char) line);
-                f2.write(line);
-                line = f1.read();
+            // Read each character and write it
+            int character = f1.read();
+            while (character != -1) {
+                System.out.print((char) character);
+                f2.write(character);
+                character = f1.read();
             }
 
             f1.close();
