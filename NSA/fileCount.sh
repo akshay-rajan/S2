3efe327736bb3d041b53056cdc446de7fb593a35
Akshay Rajan <akshayrajan345@gmail.com>
2024-06-21 22:48:00 +0530

    scripts

--- NSA/fileCount.sh
+++ NSA/fileCount.sh
@@ -2,10 +2,11 @@
 
 read -p "Directory: " dir
 direct="E:./$dir"
-
-if [ -d $direct ]; then
-    num_files=$(find $direct -type f | ls -l $direct| wc -l)
-    echo "There are $num_files files in $direct"
+if [ -d $direct ]
+then
+num_files=$(find $direct -type f | ls -l $direct|
+wc -l)
+echo "There are $num_files files in $direct"
 else
-    echo "$direct is not a directory"
-fi
+echo "$direct is not a directory"
+f
