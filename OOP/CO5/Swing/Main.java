99ea84b20838ea030c9a863a96e0611ad489c6fd
Akshay Rajan <akshayrajan345@gmail.com>
2024-06-23 22:49:00 +0530

    Frame by creating an object

---
+++ OOP/CO5/Swing/Main.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame();
+
+        // Configure the frame
+        frame.setTitle("JFrame Title");
+        frame.setVisible(true);
+        frame.setSize(420, 420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        // Change logo at the top left
+        ImageIcon logo = new ImageIcon("../../Others/favicon2.png");
+        frame.setIconImage(logo.getImage());
+
+        // Background Color (HEX or RGB)
+        frame.getContentPane().setBackground(new Color(0x123456));
+
+    }
+    
+}
