18dc6a2c31e4d78263a2bbe4ebe043ec123f756f
Akshay R <akshayrajan345@gmail.com>
2024-07-18 19:07:37 +0530

    Optimized sub-optimal code

--- OOP/CO4/Authentication.java
+++ OOP/CO4/Authentication.java
@@ -3,31 +3,41 @@
 import java.util.Scanner;
 
 public class Authentication {
+
+    // Actual username and password of the user
+    static final String USERNAME = "username";
+    static final String PASSWORD = "password";
     
     public static void main(String[] args) {
         
         try {
 
+            // Read Username
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter username: ");
             String username = sc.next();
             sc.nextLine();
             
-            if (!username.equals("username")) throw new InvalidUsernameException();
+            // Validate Username
+            if (!username.equals(USERNAME)) {
+                sc.close();
+                throw new InvalidUsernameException();
+            } 
             
+            // Read Password
             System.out.print("Enter password: ");
             String password = sc.next();
             sc.nextLine();
             sc.close();
-            
-            if (!password.equals("password")) throw new InvalidPasswordException();
+
+            // Validate Password
+            if (!password.equals(PASSWORD)) throw new InvalidPasswordException();
             
             System.out.println("Logged In!");
 
-        } catch (InvalidUsernameException e) {
+        } catch (InvalidUsernameException | InvalidPasswordException e) {
+            // Mulitcatch: Catching multiple exceptions in a single catch block
             e.printStackTrace();
-        } catch (InvalidPasswordException e) {
-            e.printStackTrace();            
         } catch (Exception e) {
             return;
         }
